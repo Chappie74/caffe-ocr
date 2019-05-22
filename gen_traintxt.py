c29a95e471f319b1927741298066a158c87f7aa5
Chappie74 <seanmsingh7@gmail.com>
2019-05-21 23:51:30 -0400

    file to generate the training file

---
+++ gen_traintxt.py
@@ -0,0 +1,29 @@
+import os
+import random
+def createTrainvalTxt(baseDirDataSet):
+    buffer = ''
+    baseDir = baseDirDataSet+'/training_images'
+    for (idx,folder) in enumerate(os.listdir(baseDir)):
+        newBase = baseDir + '/' + folder
+        for filename in os.listdir(newBase):
+            name, extension = os.path.splitext(filename)
+            s = 'training_images/'+ folder +'/'+name+  '.png' + ' ' + str(idx)
+            img_file, anno = s.strip("\n").split(" ")
+            s += '\n'
+            buffer += s
+        with open(baseDirDataSet+'/ftrain.txt', 'w') as file:
+            file.write(buffer)  
+         
+        if folder == 'Z':
+            break 
+    print("Finished creating training val. Now randomizing")
+    with open('ftrain.txt','r') as source:
+        data = [ (random.random(), line) for line in source ]
+    data.sort()
+    with open('training_index.txt','w') as target:
+        for _, line in data:
+            target.write( line )
+#Usage
+
+base = os.getcwd()
+createTrainvalTxt(base)
