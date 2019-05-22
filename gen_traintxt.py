c028395a907b327fd754a76663e82814ac8732d2
Chappie74 <seanmsingh7@gmail.com>
2019-05-22 00:05:57 -0400

    Update gen_traintxt.py

--- gen_traintxt.py
+++ gen_traintxt.py
@@ -4,6 +4,8 @@ def createTrainvalTxt(baseDirDataSet):
     buffer = ''
     baseDir = baseDirDataSet+'/training_images'
     for (idx,folder) in enumerate(os.listdir(baseDir)):
+        if folder not in ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
+            continue
         newBase = baseDir + '/' + folder
         print(folder)
         for filename in os.listdir(newBase):
